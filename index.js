4f5384607042663af49260152a306a53ffe71c62
suziwen <suziwen1@gmail.com>
2017-04-19 15:04:10 +0800

    fix write async bug

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ var util = require('util');
 var Blob = require('blob');
 var URL = global.URL || global.webkitURL || global.mozURL;
 
-function FileSystemStream(fileEntry, writer) {
+function FileSystemStream(fileEntry) {
   if (!(this instanceof FileSystemStream))
     return new FileSystemStream;
     
   WritableStream.call(this);
   this.fileEntry = fileEntry;
-  this.writer = writer;
   this.length = 0;
 }
 
@@ -20,9 +19,22 @@ FileSystemStream.prototype._write = function(chunk, encoding, callback) {
   if (!(chunk instanceof Uint8Array))
     chunk = new Uint8Array(chunk);
     
-  this.length += chunk.length;
-  this.writer.write(new Blob([chunk], {type: 'application/octet-stream'}))
-  callback();
+  if (chunk.length > 0){
+    var that = this;
+    this.fileEntry.createWriter(function(writer){
+      that.length += chunk.length;
+      writer.seek(writer.length)
+      writer.onwriteend = function(){
+        setTimeout(function(){
+          callback();
+          writer.onwriteend = null;
+        },100);
+      }
+      writer.write(new Blob([chunk], {type: 'application/octet-stream'}))
+    }, function(err){console.log(err);});
+  } else {
+    callback();
+  }
 };
 
 module.exports = FileSystemStream;
