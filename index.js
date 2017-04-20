9eb94eb25d157ffe5ce06cb7f8abc5289e72532b
suziwen <suziwen1@gmail.com>
2017-04-20 09:33:03 +0800

    保留原来的 blob stream

--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-var WritableStream = require('stream').Writable;
-var util = require('util');
-var Blob = require('blob');
-var URL = global.URL || global.webkitURL || global.mozURL;
+var fileSystemStream = require('./file-system-stream');
+var blobStream = require('./blob-stream')
 
-function FileSystemStream(fileEntry) {
-  if (!(this instanceof FileSystemStream))
-    return new FileSystemStream;
-    
-  WritableStream.call(this);
-  this.fileEntry = fileEntry;
-  this.length = 0;
+module.exports = {
+  FileSystemStream: fileSystemStream,
+  BlobStream: blobStream
 }
-
-util.inherits(FileSystemStream, WritableStream);
-
-FileSystemStream.prototype._write = function(chunk, encoding, callback) {
-  // convert chunks to Uint8Arrays (e.g. Buffer when array fallback is being used)
-  if (!(chunk instanceof Uint8Array))
-    chunk = new Uint8Array(chunk);
-    
-  if (chunk.length > 0){
-    var that = this;
-    this.fileEntry.createWriter(function(writer){
-      that.length += chunk.length;
-      writer.seek(writer.length)
-      writer.onwriteend = function(){
-        setTimeout(function(){
-          callback();
-          writer.onwriteend = null;
-        },100);
-      }
-      writer.write(new Blob([chunk], {type: 'application/octet-stream'}))
-    }, function(err){console.log(err);});
-  } else {
-    callback();
-  }
-};
-
-module.exports = FileSystemStream;
